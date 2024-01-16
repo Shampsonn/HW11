396ff159f98626a9d41055d206007d31e7002472
Alexander Bortsov <A.bortsovv@yandex.ru>
2024-01-16 20:46:40 +0300

    Initial commit

---
+++ src/main/java/ru/netology/stat/MovieManager.java
@@ -0,0 +1,20 @@
+package ru.netology.stat;
+
+public class MovieManager {
+
+    private String[] movies = new String[0];
+
+    public void addMovie(String movie) {
+        String[] tmp = new String[movies.length + 1];
+
+    }
+
+    public String[] findAll() {
+
+    }
+
+ //   public String[] findLast(){
+ //
+  //  }
+
+}
