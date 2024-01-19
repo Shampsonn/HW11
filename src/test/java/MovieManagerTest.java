90fa9c400f14b199149e0a78c816dc2fcb6a2a6e
Alexander Bortsov <A.bortsovv@yandex.ru>
2024-01-19 21:33:32 +0300

    Add findLastTest 4,5

--- src/test/java/MovieManagerTest.java
+++ src/test/java/MovieManagerTest.java
@@ -66,4 +66,35 @@ public class MovieManagerTest {
         Assertions.assertArrayEquals(expected, actual);
     }
 
+    @Test
+    public void findLastTest4() {
+        MovieManager manager = new MovieManager(6);
+        manager.addMovie("Film 1");
+        manager.addMovie("Film 2");
+        manager.addMovie("Film 3");
+        manager.addMovie("Film 4");
+
+        String[] expected = {"Film 4", "Film 3", "Film 2", "Film 1"};
+        String[] actual = manager.findLast();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void findLastTest5() {
+        MovieManager manager = new MovieManager(6);
+        manager.addMovie("Film 1");
+        manager.addMovie("Film 2");
+        manager.addMovie("Film 3");
+        manager.addMovie("Film 4");
+        manager.addMovie("Film 5");
+        manager.addMovie("Film 6");
+        manager.addMovie("Film 7");
+        manager.addMovie("Film 8");
+
+        String[] expected = {"Film 8", "Film 7", "Film 6", "Film 5", "Film 4", "Film 3"};
+        String[] actual = manager.findLast();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+
 }
